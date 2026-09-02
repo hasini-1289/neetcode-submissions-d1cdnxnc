79838cbb22177e8d6c633606548fb7d67ba6bb55
hasini-1289 <hasiniderangulaa@gmail.com>
2026-09-02 07:44:19 +0530

    Add: n-queens-ii - submission-0

---
+++ Data Structures & Algorithms/n-queens-ii/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    int n ;
+    int count =0;
+    boolean []col;
+    boolean[] diag1;
+    boolean[] diag2;
+    public int totalNQueens(int n) {
+        this.n = n;
+        col = new boolean[n];
+        diag1 = new boolean[2*n-1];
+        diag2 = new boolean[2*n-1];
+        backtrack(0);
+        return count;
+    }
+    private void backtrack(int row ){
+        if(row == n){
+            count++;
+            return;
+        }
+        for(int c=0;c<n;c++){
+            int d1 = row - c + n-1;
+            int d2 = row + c;
+            if(col[c] || diag1[d1]|| diag2[d2]) continue;
+            col[c] = true;
+            diag1[d1] = true;
+            diag2[d2] = true;
+            backtrack(row+1);
+            col[c] =false;
+            diag1[d1] = false;
+            diag2[d2] = false;
+        }
+    }
+}
